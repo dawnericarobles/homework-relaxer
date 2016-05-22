f36d37d4cf462132acfcac8bd7ecc59ed0c68b83
DAWN ROBLES <dawnericarobles@gmail.com>
2016-05-21 18:02:51 -0700

    added scripts for read more

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+
+	$("#read1").click(showMore1);
+	$("#read2").click(showMore2);
+
+		function showMore1() {
+			event.preventDefault();	
+
+			if ($("#pHidden1").hasClass("hidden")) {
+				$("#pHidden1").removeClass("hidden");
+				$("#pHidden1").slideDown();
+				// $("#pHidden1").show();
+				$("#read1").text("Read Less...");
+			} else {
+				$("#pHidden1").addClass("hidden");
+				$("#pHidden1").slideUp();
+				// $("#pHidden1").hide();
+				$("#read1").text("Read more >");
+			}
+		}
+
+		function showMore2() {
+			event.preventDefault();	
+
+			if ($("#pHidden2").hasClass("hidden")) {
+				$("#pHidden2").removeClass("hidden");
+				// $("#pHidden2").slideDown();
+				$("#pHidden2").show();
+				$("#read2").text("Read Less...");
+			} else {
+				$("#pHidden2").addClass("hidden");
+				// $("#pHidden2").slideUp();
+				$("#pHidden2").hide();
+				$("#read2").text("Read more >");
+			}
+		}
+		
+
+});
